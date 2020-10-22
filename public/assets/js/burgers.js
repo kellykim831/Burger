dfafe7d2ecb04f545d5b72a16897257627aa98b9
Kelly Kim <kellykim408@gmail.com>
2020-10-22 11:48:38 -0700

    add delete function

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-devour").on("click", function(event) {
+$(function () {
+  $(".change-devour").on("click", function (event) {
     console.log("Eating the burger now!");
     var id = $(this).data("id");
     var newDevour = $(this).data("newdevour");
@@ -12,14 +12,14 @@ $(function() {
       type: "PUT",
       data: newDevourState
     }).then(
-      function() {
+      function () {
         console.log("changed devour to", newDevour);
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
-  $(".create-form").on("submit", function(event) {
+  $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
     var newBurger = {
@@ -31,8 +31,22 @@ $(function() {
       type: "POST",
       data: newBurger
     }).then(
-      function() {
+      function () {
         console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+  $(".delete-burger").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function () {
+        console.log("deleted burger", id);
         // Reload the page to get the updated list
         location.reload();
       }
