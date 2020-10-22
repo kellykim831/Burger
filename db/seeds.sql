128baec421aa7835abd7f4dbbc3d8d40b3ac195c
Kelly Kim <kellykim408@gmail.com>
2020-10-21 19:07:05 -0700

    add seeds, schema and connection.js files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Portobello Mushroom Burgers');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burgers');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burgers', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kobe Beef Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Ahi Tuna Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
